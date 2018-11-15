318aa6d2e9566e1e77c5061f81db3cd08b59a9f8
Michelle Montano <m.montano@cca.edu>
2018-11-14 16:08:36 -0800

    help

--- clock/script.js
+++ clock/script.js
@@ -1,49 +1,67 @@
-$(document).ready(function(){
+let clockEl = document.querySelector('#clock')
+let secondHand = document.querySelector('#secondHand')
 
-    // add a zero to times below 10
-    function addZero(i) {
-        if (i < 10) {
-            i = "0" + i;
-        }
-        return i;
+// run getTime once on load
+
+getTime();
+
+// call getTime every 1 second
+
+setInterval(function() {
+  getTime();
+}, 1000);
+
+let hourElement = document.querySelector('.hour')
+
+let minuteElement = document.querySelector('.minute')
+
+let secondElement = document.querySelector('.second')
+
+
+
+// set up time getting function
+
+function getTime(){
+  hourElement.style.transform = 'rotate('+( h * 30 )+'deg)'
+
+   minuteElement.style.transform = 'rotate('+( m * 30 )+'deg)'
+
+    secondElement.style.transform = 'rotate('+( h * 30 )+'deg)'
+}
+
+  // get the date
+
+  let d = new Date();
+
+  // get hours, minutes, and seconds
+
+  let h = addZero(d.getHours());
+  let m = addZero(d.getMinutes());
+  let s = addZero(d.getSeconds());
+
+  // update the clock's text
+
+  // let timeText = h + ":" + m + ":" + s
+  // clockEl.innerText = timeText
+
+  // rotate secondHand based on the second
+
+  secondHand.animate([{
+    transform:'rotate('+ (s-1) * 6 +'deg)'
+  },
+  {
+    transform:'rotate('+ s * 6 +'deg)'
+  }],{
+    duration:100,
+    easing:'cubic-bezier(0.75, 0.000, 0.800, 1.5)',
+    fill:'forwards'
+  })
+
+// add a zero to times below 10
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
     }
-
-    // set up time getting function
-    function getTime(){
-        // get the date
-    	  var d = new Date();
-
-        // get hours, minutes, and seconds
-        var h = addZero(d.getHours());
-        var m = addZero(d.getMinutes());
-        var s = addZero(d.getSeconds());
-
-        // update the clock's text
-        $('.hour').html(h);
-        $('.minute').html(m);
-        $('.second').html(s);
-
-        // rotate clock
-        $('.hour').css('transform', 'rotate('+ h * 6 +'deg');
-        $('.minute').css('transform', 'rotate('+ m * 6 +'deg');
-        $('.second').css('transform', 'rotate('+ s * 6 +'deg');
-
-        // detect a specific time
-        if(m == 43){
-        	$('body').addClass('noon');
-        } else {
-        	$('body').removeClass('noon');
-        }
-    }
-
-    // call getTime every 1 second
-    setInterval(function() {
-      getTime();
-    }, 1000);
-
-    setInterval(function() {
-      $('body').append('test');
-    }, 60000);
-
-
-});
+    return i;
+}
